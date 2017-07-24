1c5fe08009a14e02b89f7e48586a6dcf204ac0af
Attila Kun <kun.attila.hun@gmail.com>
2017-07-25 00:33:08 +0100

    Started to load images.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,28 @@
-var message: String = 'hello';
-console.log(message);
+function loadImage() {
+  var resolve: (value: any) => void, reject;
+
+  var promise: Promise<HTMLImageElement> = new Promise((res, rej) => {
+    resolve = res;
+    reject = rej;
+  });
+
+  var image = new Image();
+  image.onload = onImageLoaded;
+  image.src = 'samples/Village.png';
+
+  function onImageLoaded() {
+    resolve(image);
+  }
+
+  return promise;
+}
+
+async function init() {
+  var img = await loadImage();
+  var canvas = <HTMLCanvasElement>document.getElementById("canvas");
+  var context = canvas.getContext("2d");
+  context.drawImage(img, 0, 0);
+  console.log(img.src);
+}
+
+init();
